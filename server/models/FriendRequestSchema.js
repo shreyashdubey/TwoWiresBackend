2a8968a9cdf531fb05ddcabcae1daa60b4ef1800
shreyashdubey <shreyashlrn@gmail.com>
2023-11-02 07:18:21 +0530

    fix: Changed Friend Schema

--- server/models/FriendRequestSchema.js
+++ server/models/FriendRequestSchema.js
@@ -3,22 +3,10 @@ const mongoose = require('mongoose');
 
 const friendRequestSchema = new mongoose.Schema(
   {
-    sender: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'User',
-      required: true,
-    },
-    receiver: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'User',
-      required: true,
-    },
-    status: {
-      type: String,
-      enum: ['pending', 'accepted', 'rejected'],
-      default: 'pending',
-    },
-  },
+    sender: {type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true,},
+    receiver: {type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true,},
+    status: {type: String, enum: ['pending', 'accepted', 'rejected'], default: 'pending',},
+},
   { timestamps: true }
 );
 
--- server/models/FriendSchema.js
+++ server/models/FriendSchema.js
@@ -1,18 +1,13 @@
-// models/Friend.js
 const mongoose = require('mongoose');
 
 const friendSchema = new mongoose.Schema({
-  user: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',
-    required: true,
-  },
-  friends: [{
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',
-  }],
+  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  friends: [
+    {
+      friendUser: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+      friendshipDate: { type: Date, default: Date.now }
+    }
+  ]
 });
-
 const Friend = mongoose.model('Friend', friendSchema);
-
 module.exports = Friend;
