ef963512c5b9220c19dc44fcdd79b614e62dfacc
shreyashdubey <shreyashlrn@gmail.com>
2023-11-11 18:57:04 +0530

    add: give userId on Login

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -90,7 +90,7 @@ router.post(
       
       const accessToken = generateAccessToken ({user: user})
       const refreshToken = generateRefreshToken ({user: user})
-      res.json ({accessToken: accessToken, refreshToken: refreshToken})
+      res.json ({accessToken: accessToken, refreshToken: refreshToken, userId: user._id})
 
       // const token = createSecretToken(user._id);
       // res.cookie("token", token, {
