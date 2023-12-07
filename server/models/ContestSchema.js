5af640d47e1fbfcef74efe867a52d27aa0abed56
shreyashdubey <shreyashlrn@gmail.com>
2023-12-07 14:39:07 +0530

    add: get contest routes

--- server/models/ContestSchema.js
+++ server/models/ContestSchema.js
@@ -1,21 +1,30 @@
-// models/problemSchema.js
 const mongoose = require('mongoose');
-const AiOptions = require('../enums/AiOptions');
 
-const contestSchema = new mongoose.Schema({
-  contestName: { type: String, required: true},
-  contestOrganizer: {type: String},
-  contestCreator: [{type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true}],
-  contestDescription: {type: mongoose.Schema.Types.ObjectId, ref: 'ContestDescriptionSchema'},
-  totalTeamsRegistered: {type: Number, default: 0},
-  isDeleted: {type: Boolean, default: false},
-  startTime: {type: Date, required: false},
-  endTime: {type: Date, required: false},
-  isPublished: {type: Boolean, default: false},
-  prize: {type: String}
-},
-{
-    timestamps: true
+const contestSchema = new mongoose.Schema(
+  {
+    contestName: { type: String, required: true },
+    contestOrganizer: { type: String },
+    contestCreator: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }],
+    contestDescription: { type: mongoose.Schema.Types.ObjectId, ref: 'ContestDescriptionSchema' },
+    totalTeamsRegistered: { type: Number, default: 0 },
+    isDeleted: { type: Boolean, default: false },
+    startTime: { type: Date, required: false },
+    endTime: { type: Date, required: false },
+    isPublished: { type: Boolean, default: false },
+    prize: { type: String },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+// Adding pre-save middleware for date validation
+contestSchema.pre('save', function (next) {
+  if (this.startTime && this.endTime && this.startTime > this.endTime) {
+    const error = new Error('End date must be greater than or equal to start date.');
+    return next(error);
+  }
+  next();
 });
 
 const ContestSchema = mongoose.model('ContestSchema', contestSchema);
--- server/routes/contestRoutes.js
+++ server/routes/contestRoutes.js
@@ -77,7 +77,42 @@ router.post('/add-contest-creator/:contestId', async (req, res) => {
     }
   });
   
-// Delete contest
+
+// Edit contest route
+router.put('/edit-contest/:contestId', async (req, res) => {
+  try {
+    const { contestName, contestOrganizer, startTime, endTime, isPublished, prize } = req.body;
+    const { contestId } = req.params;
+
+    // Check if the contest with the provided ID exists
+    const existingContest = await Contest.findById(contestId);
+
+    if (!existingContest) {
+      return res.status(404).json({ error: `Contest with ID ${contestId} not found` });
+    }
+
+    if (contestName && !contestName.trim()) {
+      return res.status(400).json({ error: 'Contest name cannot be empty or contain only whitespace' });
+    }
+
+    // Update the contest fields
+    existingContest.contestName = contestName || existingContest.contestName;
+    existingContest.contestOrganizer = contestOrganizer || existingContest.contestOrganizer;
+    existingContest.startTime = startTime || existingContest.startTime;
+    existingContest.endTime = endTime || existingContest.endTime;
+    existingContest.isPublished = isPublished || existingContest.isPublished;
+    existingContest.prize = prize || existingContest.prize;
+
+    // Save the updated contest
+    const updatedContest = await existingContest.save();
+
+    res.status(200).json({ success: true, message: 'Contest updated successfully', contest: updatedContest });
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+});
+
+  // Delete contest
 router.delete('/delete-contest/:contestId/:adminId', async (req, res) => {
     const { contestId, adminId } = req.params;
     try {
@@ -106,5 +141,80 @@ router.delete('/delete-contest/:contestId/:adminId', async (req, res) => {
       res.status(500).json({ errors: [{ msg: 'Server error' }] });
     }
   });
+
+// Get all contests created by a user
+router.get('/get-contests-by-user/:user', async (req, res) => {
+  try {
+    const { user } = req.params;
+    const { page, pageSize } = req.query;
+
+    const pageOptions = {
+      page: parseInt(page, 10) || 1,
+      pageSize: parseInt(pageSize, 10) || 10,
+    };
+
+    const skip = (pageOptions.page - 1) * pageOptions.pageSize;
+
+    // Check if the user exists
+    const userExists = await User.findById(user);
+    if (!userExists) {
+      return res.status(404).json({ error: 'User not found' });
+    }
+
+    const contests = await Contest.find({ contestCreator: user, isDeleted: false })
+      .skip(skip)
+      .limit(pageOptions.pageSize)
+      .populate('contestCreator', 'username') // Assuming 'username' is a field in your User model
+      .populate('contestDescription', 'description'); // Assuming 'description' is a field in your ContestDescriptionSchema
+
+    const totalContests = await Contest.countDocuments({ contestCreator: user, isDeleted: false });
+
+    const totalPages = Math.ceil(totalContests / pageOptions.pageSize);
+
+    res.status(200).json({
+      contests,
+      page: pageOptions.page,
+      pageSize: pageOptions.pageSize,
+      totalPages,
+      totalContests,
+    });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+// Get all contests - sorted by date (desc) and published
+router.get('/get-all-contests', async (req, res) => {
+  try {
+    const { page, pageSize } = req.query;
+
+    const pageOptions = {
+      page: parseInt(page, 10) || 1,
+      pageSize: parseInt(pageSize, 10) || 10,
+    };
+
+    const skip = (pageOptions.page - 1) * pageOptions.pageSize;
+
+    const contests = await Contest.find({ isDeleted: false, isPublished: true })
+      .sort({ createdAt: 'desc' }) // Sort by creation date in descending order
+      .skip(skip)
+      .limit(pageOptions.pageSize)
+      .populate('contestCreator', 'username') // Assuming 'username' is a field in your User model
+
+    const totalContests = await Contest.countDocuments({ isDeleted: false });
+
+    const totalPages = Math.ceil(totalContests / pageOptions.pageSize);
+
+    res.status(200).json({
+      contests,
+      page: pageOptions.page,
+      pageSize: pageOptions.pageSize,
+      totalPages,
+      totalContests,
+    });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
 
 module.exports = router;
