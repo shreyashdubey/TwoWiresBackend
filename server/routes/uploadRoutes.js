0e4de3c9368533c876fe8e721e8e67d952174b64
shreyashdubey <shreyashlrn@gmail.com>
2023-12-15 14:12:28 +0530

    fix: Pdf details get added

--- server/routes/uploadRoutes.js
+++ server/routes/uploadRoutes.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const multer = require('multer')
+const PdfDetails = require('../models/PdfDetailsSchema');
+app.use("/files", express.static("files"));
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
@@ -16,4 +18,23 @@ const upload = multer({ storage : storage})
 
 app.post('/upload-files',upload.single("file"),async(req, res) => {
     console.log(req.file);
-})
+    const title = req.body.title;
+    const fileName = req.file.filename;
+    try {
+      await PdfDetails.create({ title: title, pdf: fileName });
+      res.send({ status: "ok" });
+    } catch (error) {
+      res.json({ status: error });
+    }
+});
+
+app.get("/get-files", async (req, res) => {
+  try {
+    PdfDetails.find({}).then((data) => {
+      res.send({ status: "ok", data: data });
+    });
+  } catch (error) {}
+});
+
+
+module.exports = router;
