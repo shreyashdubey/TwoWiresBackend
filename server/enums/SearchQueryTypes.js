c7c088eae60c4129127d79325091b46d2cd87a35
shreyashdubey <shreyashlrn@gmail.com>
2023-12-28 23:34:36 +0530

    add: Search Query Types

---
+++ server/enums/SearchQueryTypes.js
@@ -0,0 +1,9 @@
+const SearchQueryTypes = Object.freeze({
+    USERNAME: 'USERNAME',
+    FIRST_NAME: 'FIRST_NAME',
+    MIDDLE_NAME: 'MIDDLE_NAME',
+    LAST_NAME: 'LAST_NAME',
+    COMPANY_NAME: 'COMPANY_NAME',
+    INDUSTRY_NAME: 'INDUSTRY_NAME',
+  })
+  module.exports = SearchQueryTypes;
