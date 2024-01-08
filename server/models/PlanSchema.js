bd7912b335498a251db936fe30677146d5fe60a4
shreyashdubey <shreyashlrn@gmail.com>
2024-01-09 02:35:17 +0530

    add: Plan Schema

---
+++ server/models/PlanSchema.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const planSchema = new mongoose.Schema(
+  {
+    planName: { type: String, required: true },
+    planCreator: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    contest: { type: mongoose.Schema.Types.ObjectId, ref: 'ContestSchema' },
+    upVotes: { type: Number, default: 0 },
+    downVotes: { type: Number, default: 0 },
+    isDeleted: { type: Boolean, default: false },
+    isSubmitted: {type: Boolean, default: false},
+    isUnderReview: {type: Boolean, default: false},
+    isPublished: { type: Boolean, default: false},
+  },
+  {
+    timestamps: true,
+  }
+);
+
+
+const PlanSchema = mongoose.model('PlanSchema', planSchema);
+
+module.exports = PlanSchema;
