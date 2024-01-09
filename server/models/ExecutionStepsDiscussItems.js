3eeea766db5b58c8ed09b65757ceab160a49dd5c
shreyashdubey <shreyashlrn@gmail.com>
2024-01-09 15:18:37 +0530

    fix: upvotes logic

--- server/models/ExecutionStepsDiscussItems.js
+++ server/models/ExecutionStepsDiscussItems.js
@@ -1,20 +1,20 @@
 const DiscussItemTypes = require('../enums/DiscussItemTypes');
 const mongoose = require('mongoose');
 
-const commentSchema = new mongoose.Schema({
+const executionStepsDiscussItems = new mongoose.Schema({
   user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
   plan: { type: mongoose.Schema.Types.ObjectId, ref: 'PlanSchema', required: true},
   itemType: {type: String, enum: DiscussItemTypes, required: true},
   text: { type: String, required: true },
   path: { type: String, required: true }, // Materialized Path creation logic in routes
-  upVotes: {type: Number, default: 0, required: true},
-  downVotes: {type: Number, default: 0, required: true},
+  upVotes: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User'}],
+  downVotes: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User'}],
   isDeleted: {type: Boolean, default: false},
 },
 {
     timestamps: true,
 });
 
-const PostComment = mongoose.model('PostComment', commentSchema);
+const ExecutionStepsDiscussItems = mongoose.model('ExecutionStepsDiscussItems', executionStepsDiscussItems);
 
-module.exports = PostComment;
+module.exports = ExecutionStepsDiscussItems;
